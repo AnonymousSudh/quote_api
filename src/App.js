5140ec78c9557e998892bee4e50794f780b5abf3
Sudhanshu Kumar <64887608+AnonymousSudh@users.noreply.github.com>
2021-12-03 21:49:05 +0530

    func

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+   const nextjoke = async ()=>{
+      let api = await (await fetch("https://type.fit/api/quotes")).json();
+      const no = Math.round(Math.random()*1000);
+
+      console.log(no);
+      console.log(api[no].text);
+      document.getElementById('joke_holder').innerHTML = api[no].text;
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+  <div className= "main_container"> 
+    <div className = "container">
+
+    <h1 id="joke_holder"> click to quote</h1>
+    <button onClick={nextjoke}>Next</button>
     </div>
+
+
+
+  </div>
+  </>
   );
 }
 
